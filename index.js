085114c116442c95d22b2e906815d40334a6a297
John Otto <jotto88@gmail.com>
2022-11-01 20:45:18 -0400

    commit tree 209b6436e22953bbdc016da6f461db0dc658adfd

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if ( feet<= 400) {
+    return 'This one is on me!'
+  }
+  if ( feet<2000) {
+    return 'That will be twenty bucks.'
+  }
+  if ( feet>2000 && feet<=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+    else {
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(city){
+  let reply;
+  city === "NYC" ? reply = 'Ok, sounds good.' : reply = 'No go.';
+  return reply;  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tips){
+  if(tips==='generous') {
+    return 'Thank you so much.';
+  }
+  if(tips==='not as generous') {
+    return 'Thank you.';
+  }
+  if (tips === 'thanks for everything') {
+    return 'Bye.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
